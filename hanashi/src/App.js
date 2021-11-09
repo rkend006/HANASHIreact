6b3488db7e31f0c11d03f23cbe0e7de0c3b29df9
rkend006 <36499226+rkend006@users.noreply.github.com>
2021-11-09 13:18:41 -0500

    adding to hanashi site

--- hanashi/src/App.js
+++ hanashi/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const title = 'Welcome to the new Blog';
+  const likes = 50;
+  const person = {name:'Ryan',age:'26'};
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+      <div className = "content">
+        <h1>{title}</h1>
+        <p>liked {likes} times</p>
+      </div>
     </div>
+
   );
 }
 

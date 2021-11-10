8efbfacbafea37e4855d19de0c2c17bae04c2837
rkend006 <36499226+rkend006@users.noreply.github.com>
2021-11-09 20:35:44 -0500

    added some links

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ function App() {
 
   const title = 'HANASHI APPLICATION';
   const likes = 50;
-  const person = {name:'Ryan',age:'26'};
-
+  const igLink = "https://www.instagram.com/hvnvshi/"
+  const youtubeLink = "https://www.youtube.com/watch?v=1So4hbdJ6r0"
 
 
   return (
@@ -13,6 +13,10 @@ function App() {
       <div className = "content">
         <h1>{title}</h1>
         <p>liked {likes} times!</p>
+
+        <a href={igLink}>Instagram</a>
+        <p> </p>
+        <a href={youtubeLink}>YouTube</a>
       </div>
     </div>
 

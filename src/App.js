87d86dadd1500be3604baf5da7ebd62d2ae9a857
rkend006 <36499226+rkend006@users.noreply.github.com>
2021-11-09 23:47:19 -0500

    added bg

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import './App.css';
+import bg from './img/HANASHI_MAIN_PAGE_BG.png'
+import Navbar from './NavBar';
+import Home from './Home';
 
 function App() {
 
@@ -7,16 +9,180 @@ function App() {
   const igLink = "https://www.instagram.com/hvnvshi/"
   const youtubeLink = "https://www.youtube.com/watch?v=1So4hbdJ6r0"
 
+  const styles = {
+    container: {
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        width: '100vw',
+        height: '1000vh'
+    }
+  }
 
   return (
-    <div className = "App">
+
+
+    <div className = "App" style={{ backgroundImage: `url(${bg})` }} >
+      <Navbar />
       <div className = "content">
+        <Home />
         <h1>{title}</h1>
         <p>liked {likes} times!</p>
 
         <a href={igLink}>Instagram</a>
         <p> </p>
         <a href={youtubeLink}>YouTube</a>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+        <p> .</p>
+
+
       </div>
     </div>
 
